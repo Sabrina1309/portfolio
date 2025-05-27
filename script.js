8d47808f85d88a4de48f097b4e8f5ceef78e24d8
Sabrina1309 <sabrinamavlonova2007@gmail.com>
2025-05-27 21:49:53 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const resumeBtn = document.querySelector(".btn .btn-primary");
+resumeBtn.addEventListener("click", function() {
+  window.open("file:///C:/Users/Sabrina12345678/Downloads/SabrinaResume.pdf", "_blank" );
+});
