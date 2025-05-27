cf73de1471459e38b84e5371918f345a4c27591d
Sabrina1309 <sabrinamavlonova2007@gmail.com>
2025-05-27 22:14:59 +0500

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const resumeBtn = document.querySelector(".btn .btn-primary");
+const resumeBtn = document.querySelector(".btn.btn-primary");
 resumeBtn.addEventListener("click", function() {
   window.open("https://drive.google.com/file/d/1Cv8QVfeNwc1-X3zcSow12BYhep9qXidD/view?usp=drive_link", "_blank" );
 });
