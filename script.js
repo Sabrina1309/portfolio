a935381472322dc65d49a30a93e6ad07076e8864
Sabrina1309 <sabrinamavlonova2007@gmail.com>
2025-05-29 10:33:46 +0500

    script.js

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 
 
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function () {
+  // Resume button
   const resumeBtn = document.querySelector(".btn-resume");
-
-  resumeBtn.addEventListener("click", function() {
-    window.open("https://drive.google.com/file/d/1Cv8QVfeNwc1-X3zcSow12BYhep9qXidD/view?usp=drive_link", "_blank");
-  });
-
-
-
-  const linkedInButton = document.querySelector(".btn-linked");
-    linkedInButton.addEventListener("click", function() {
-      
-      window.open("https://www.linkedin.com/in/sabrina-mavlonova-2045a4364/", "_blank");
+  if (resumeBtn) {
+    resumeBtn.addEventListener("click", function () {
+      window.open(
+        "https://drive.google.com/file/d/1Cv8QVfeNwc1-X3zcSow12BYhep9qXidD/view?usp=drive_link",
+        "_blank"
+      );
     });
-
-  
-  /* function showGallery() {
-   window.open("https://sabrina1309.github.io/collegetips_gallery/", "_blank");
   }
-  
-  function showGeology() {
-   window.open(" https://sabrina1309.github.io/", "_blank");
+
+  // LinkedIn button
+  const linkedInButton = document.querySelector(".btn-linked");
+  if (linkedInButton) {
+    linkedInButton.addEventListener("click", function () {
+      window.open(
+        "https://www.linkedin.com/in/sabrina-mavlonova-2045a4364/",
+        "_blank"
+      );
+    });
   }
-  
-  function showToDo() {
-   window.open("https://sabrina1309.github.io/to-doIntern/", "_blank");
-  }
-   */  
 
-  ////sjnsjn
+  // Project buttons (used in index.html)
+  window.showGallery = function () {
+    window.open("https://sabrina1309.github.io/collegetips_gallery/", "_blank");
+  };
 
+  window.showGeology = function () {
+    window.open("https://sabrina1309.github.io/", "_blank");
+  };
 
-  
-  });
+  window.showToDo = function () {
+    window.open("https://sabrina1309.github.io/to-doIntern/", "_blank");
+  };
+});
+
   
