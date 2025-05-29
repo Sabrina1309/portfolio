b4026509825f8350415930727d997e56e4679c40
Sabrina1309 <sabrinamavlonova2007@gmail.com>
2025-05-29 09:27:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 document.addEventListener("DOMContentLoaded", function() { 
-  const resumeBtn = document.querySelector(".btn.btn-primary");
+  const resumeBtn = document.querySelector(".btn.btn-resume");
 
   resumeBtn.addEventListener("click", function() {
     window.open("https://drive.google.com/file/d/1Cv8QVfeNwc1-X3zcSow12BYhep9qXidD/view?usp=drive_link", "_blank");
